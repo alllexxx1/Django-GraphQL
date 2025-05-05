96110e24b31430ba1cd7d2378b6495c17af83934
Alexei Korpachev <alekkorpachyov@gmail.com>
2025-05-05 11:41:35 +0300

    feat: setup CORS

--- book_blog/settings.py
+++ book_blog/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'corsheaders',
     'book_blog',
     'book_blog.blog',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GRAPHENE = {
     'SCHEMA': 'book_blog.schema.schema'
 }
+
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
